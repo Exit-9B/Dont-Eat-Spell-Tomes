e905f8453d2f2df0ca34fdf1ee4794e45f8a2d10
Parapets <southwest85b@gmail.com>
2020-12-05 14:14:29 +0000

    Allow forms to register for events

--- src/Papyrus.cpp
+++ src/Papyrus.cpp
@@ -1,9 +1,13 @@
 #include "Papyrus.h"
+#include "FormExt.h"
 #include "ReferenceAliasExt.h"
 
 void Papyrus::Register()
 {
 	auto papyrus = SKSE::GetPapyrusInterface();
+
+	papyrus->Register(FormExt::RegisterFuncs);
+	logger::info("Registered DEST_FormExt"sv);
 
 	papyrus->Register(ReferenceAliasExt::RegisterFuncs);
 	logger::info("Registered DEST_ReferenceAliasExt"sv);

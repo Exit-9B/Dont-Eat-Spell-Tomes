5302fea4150397cf97ae110b4a01c2b7092bde2a
Parapets <southwest85b@gmail.com>
2020-12-05 19:57:39 +0000

    Move source and header lists to separate files

---
+++ cmake/headerlist.cmake
@@ -0,0 +1,13 @@
+set(headers ${headers}
+	src/PCH.h
+	src/Offsets.h
+	src/Hooks.h
+
+	src/Registration.h
+	src/Papyrus.h
+	src/FormExt.h
+	src/ReferenceAliasExt.h
+	src/UIExt.h
+
+	src/SpellTomeManager.h
+)
---
+++ cmake/sourcelist.cmake
@@ -0,0 +1,14 @@
+set(sources ${sources}
+	src/main.cpp
+	src/PCH.cpp
+	src/Hooks.cpp
+
+	src/Registration.cpp
+	src/Papyrus.cpp
+
+	src/FormExt.cpp
+	src/ReferenceAliasExt.cpp
+	src/UIExt.cpp
+
+	src/SpellTomeManager.cpp
+)
